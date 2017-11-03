9dcda4d7fe1a12bb12b265fb8a0a28b92201f21d
aczech <aczech@lab011.elka.pw.edu.pl>
2017-11-03 08:54:42 +0100

    version1

---
+++ Liniowe/Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mion/s/166/aczech/Desktop/Liniowe
+# Build directory: /home/mion/s/166/aczech/Desktop/Liniowe/Debug
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(src)
+SUBDIRS(tests)
---
+++ Liniowe/Release/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mion/s/166/aczech/Desktop/Liniowe/tests
+# Build directory: /home/mion/s/166/aczech/Desktop/Liniowe/Release/tests
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(boostUnitTestsRun "aisdiLinearTests")
